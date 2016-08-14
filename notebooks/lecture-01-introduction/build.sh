6146dde3941cc786d30edecd732752e2c0f8c215
Anders Johannsen <anders@johannsen.com>
2016-08-14 19:37:56 +0200

    add correct URL for mathjax

--- notebooks/lecture-01-introduction/build.sh
+++ notebooks/lecture-01-introduction/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 name=$(echo $1 | cut -f 1 -d '.')
-pandoc --standalone --slide-level 2 --variable width="1600" --variable height="1200" --variable theme="beige" --mathjax  -t revealjs+raw_html --bibliography refs.bib $1 -o ${name}.html
+pandoc --standalone --slide-level 2 --variable width="1600" --variable height="1200" --variable theme="white" \
+--mathjax="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" \
+  -t revealjs+raw_html --bibliography refs.bib $1 -o ${name}.html
